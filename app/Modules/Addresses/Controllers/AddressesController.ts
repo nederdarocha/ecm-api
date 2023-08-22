f2dddda0eac9341ac9f697102fb1822bcbf7a1b7
Izidorio Bento <izidoriojr@gmail.com>
2023-08-22 08:00:07 -0300

    chore

--- app/Modules/Addresses/Controllers/AddressesController.ts
+++ app/Modules/Addresses/Controllers/AddressesController.ts
@@ -1,6 +1,7 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { CustomerValidator } from "../Validators";
 import Address from "../Models/Address";
+import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class AddressesController {
   public async addressesOwner({ auth, params: { id } }: HttpContextContract) {
@@ -8,6 +9,24 @@ export default class AddressesController {
       .where("tenant_id", auth.user!.tenant_id)
       .andWhere("owner_id", id)
       .orderBy("favorite", "desc");
+  }
+
+  public async addressesFavorite({ auth, response, params: { id } }: HttpContextContract) {
+    const address = await Address.query()
+      .where("tenant_id", auth.user!.tenant_id)
+      .andWhere("id", id)
+      .firstOrFail();
+
+    await Database.rawQuery(
+      `
+      UPDATE addresses SET favorite = false
+      WHERE tenant_id = :tenant_id AND owner_id = :owner_id
+    `,
+      { tenant_id: auth.user!.tenant_id, owner_id: address.owner_id! }
+    );
+
+    await address.merge({ favorite: true }).save();
+    return response.ok({ message: "Endereço favoritado com sucesso!" });
   }
 
   public async index({ auth, paginate }: HttpContextContract) {
--- app/Modules/Addresses/Routes/AddressesRoutes.ts
+++ app/Modules/Addresses/Routes/AddressesRoutes.ts
@@ -3,11 +3,11 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.where("id", Route.matchers.uuid());
 
 Route.group(() => {
-  Route.get("addresses/owner/:id", "AddressesController.addressesOwner");
-
   Route.group(() => {
+    Route.get("addresses/owner/:id", "AddressesController.addressesOwner");
+    Route.get("addresses/favorite/:id", "AddressesController.addressesFavorite");
     Route.resource("addresses", "AddressesController").apiOnly();
-  }).middleware(["acl:crud-address"]);
+  }).middleware(["acl:crud-address", "sleep:1000"]);
 })
   .middleware(["auth"])
   .namespace("App/Modules/Addresses/Controllers");
