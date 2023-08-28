d6ca2ede0aac1815cad0e4274fcdcd425895d581
Izidorio Bento <izidoriojr@gmail.com>
2023-08-28 13:56:40 -0300

    add migrations categories e services

--- database/migrations/1693234527512_categories.ts
+++ database/migrations/1693234527512_categories.ts
@@ -1,18 +1,14 @@
 import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = "services";
+  protected tableName = "categories";
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid("id").primary().defaultTo(this.db.rawQuery("uuid_generate_v4()").knexQuery);
       table.uuid("tenant_id").references("id").inTable("tenants").notNullable().index();
-      table.uuid("category_id").references("id").inTable("categories").onDelete("CASCADE");
       table.string("name");
       table.string("description");
-      table.integer("price");
-      table.integer("commission");
-      table.boolean("out_court").defaultTo(false);
       table.boolean("status").defaultTo(true);
 
       table.unique(["name", "tenant_id"]);
