4841ac836ba07db6ecdd29008b7ecd12eeb1701b
Izidorio Bento <izidoriojr@gmail.com>
2025-08-08 08:06:48 -0300

    chore

---
+++ database/perfil-acesso.sql
@@ -0,0 +1,32 @@
+INSERT INTO permissions (id,"name",slug,c,r,u,d,description,created_at,updated_at) VALUES
+	 ('8b45584e-7e12-4f62-8e36-9a78f8a702d8','Competências','cru-court',true,true,true,false,NULL,'2023-11-15 19:15:04.842-03','2023-11-21 06:54:43.93-03'),
+	 ('4504d63a-f8fe-4b33-a42a-66bcbd91fb79','Andamentos','cru-task',true,true,true,false,NULL,'2023-10-18 22:20:07.617-03','2024-01-11 05:55:33.571-03'),
+	 ('27eb151a-f45d-4b4f-9963-d957a5509bf7','Endereços','cru-address',true,true,true,false,NULL,'2023-11-15 19:13:26.442-03','2023-11-15 19:13:26.442-03'),
+	 ('8894ed55-262f-40d7-8b04-592dfe84ef4b','Clientes','cru-customer',true,true,true,false,NULL,'2023-11-15 19:15:51.184-03','2023-11-15 19:15:51.185-03'),
+	 ('95b18932-31ac-4d81-a5ec-d0e12ad22691','Arquivos','cru-file',true,true,true,false,NULL,'2023-11-15 19:16:55.442-03','2023-11-15 19:16:55.442-03'),
+	 ('54bad2ec-b817-46fb-baa0-c1e92bb6cfc9','Mensagens','cru-message',true,true,true,false,NULL,'2023-10-18 22:19:35.678-03','2023-11-15 19:17:27.912-03'),
+	 ('14e89f18-1bb5-4469-8050-9c261690bcec','Contratos','cru-order',true,true,true,false,NULL,'2023-10-18 22:19:17.696-03','2023-11-15 19:17:30.92-03'),
+	 ('b65436ae-dc9a-4db9-a69f-b1aab5f668f4','Status','cru-status',true,true,true,false,NULL,'2023-10-18 22:19:49.766-03','2023-11-15 19:18:15.409-03'),
+	 ('2ef6154b-a018-495f-809e-c5e23dd11e0f','Pagamentos','cru-payment',true,true,true,false,NULL,'2023-10-18 22:20:23.343-03','2023-11-15 19:19:20.408-03'),
+	 ('654e4f21-d0de-4d2e-84ef-5a9c6d319751','Categorias','cru-category',true,true,true,false,NULL,'2023-10-18 22:21:02.22-03','2023-11-15 19:19:49.273-03'),
+	 ('9290f077-f099-4dbe-af75-21d9adfbdd21','Dados Extras','cru-extra_data',true,true,true,false,NULL,'2023-10-18 22:21:21.137-03','2023-11-15 19:20:10.182-03'),
+	 ('d1b7bdc2-0666-4bef-8b3d-e3dbf0fa3b6f','Serviços','cru-service',true,true,true,false,NULL,'2023-10-18 22:20:44.956-03','2023-11-15 19:20:33.238-03'),
+	 ('bf3afd86-cd2e-4475-b53e-cfce004b4f9f','Usuarios','-user',false,false,false,false,NULL,'2023-11-15 19:21:21.829-03','2023-11-15 19:21:23.753-03'),
+	 ('217f038c-5b3a-4ef7-a57e-2647d6310445','Meta Dados','cru-meta_data',true,true,true,false,NULL,'2023-10-18 22:21:36.799-03','2023-11-15 19:21:27.553-03');
+
+
+INSERT INTO permissions (id,"name",slug,c,r,u,d,description,created_at,updated_at) VALUES
+('73607617-8eb0-48f8-ae6b-c1dd615207a6','Competências','cru-court',true,true,true,false,'auxiliar','2023-11-15 19:15:04.842-03','2023-11-21 06:54:43.93-03'),
+('db1cb4ce-f6d0-4684-9bdf-8787fc07dee9','Andamentos','cru-task',true,true,true,false,'auxiliar','2023-10-18 22:20:07.617-03','2024-01-11 05:55:33.571-03'),
+('735e4469-1e66-40a0-b5d0-b6e93b47a6c1','Endereços','cru-address',true,true,true,false,'auxiliar','2023-11-15 19:13:26.442-03','2023-11-15 19:13:26.442-03'),
+('d755e1b2-42c3-4f5e-abb8-df564be40b97','Clientes','cru-customer',true,true,true,false,'auxiliar','2023-11-15 19:15:51.184-03','2023-11-15 19:15:51.185-03'),
+('37f6e4d0-9ac3-4458-abf6-242227c5733d','Arquivos','cru-file',true,true,true,false,'auxiliar','2023-11-15 19:16:55.442-03','2023-11-15 19:16:55.442-03'),
+('e3df36da-5740-447e-a3bf-d3b66b885602','Mensagens','cru-message',true,true,true,false,'auxiliar','2023-10-18 22:19:35.678-03','2023-11-15 19:17:27.912-03'),
+('961aabea-a2b6-46ad-a3b2-0c02caa5adc9','Contratos','cru-order',true,true,true,false,'auxiliar','2023-10-18 22:19:17.696-03','2023-11-15 19:17:30.92-03'),
+('4ee0cac0-e4c8-4d4f-821a-d1e0c8e29a52','Status','cru-status',true,true,true,false,'auxiliar','2023-10-18 22:19:49.766-03','2023-11-15 19:18:15.409-03'),
+('9268947f-381b-4554-ac2a-ac4c8585ed9f','Pagamentos','cru-payment',true,true,true,false,'auxiliar','2023-10-18 22:20:23.343-03','2023-11-15 19:19:20.408-03'),
+('303fdc63-af2f-4f2a-a9ec-c19e33ea17cc','Categorias','cru-category',true,true,true,false,'auxiliar','2023-10-18 22:21:02.22-03','2023-11-15 19:19:49.273-03'),
+('e9320d49-159b-473c-95cc-1ff675730fe9','Dados Extras','cru-extra_data',true,true,true,false,'auxiliar','2023-10-18 22:21:21.137-03','2023-11-15 19:20:10.182-03'),
+('80739cc3-a4d3-4750-87e7-8b62e68a62dd','Serviços','cru-service',true,true,true,false,'auxiliar','2023-10-18 22:20:44.956-03','2023-11-15 19:20:33.238-03'),
+('22e702ec-4ad3-4759-8288-1f3bc741b24b','Usuarios','-user',false,false,false,false,'auxiliar','2023-11-15 19:21:21.829-03','2023-11-15 19:21:23.753-03'),
+('5272c32c-680d-4f0d-98e5-82b435d3cfeb','Meta Dados','cru-meta_data',true,true,true,false,'auxiliar','2023-10-18 22:21:36.799-03','2023-11-15 19:21:27.553-03');
